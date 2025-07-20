1fc387b5cd1a83f9da6ec39ffb8a97c559a8bb75
YTeyddie22 <teyddie@gmail.com>
2025-07-20 10:53:44 +0300

    fix:  routing using declarative mode

--- Bookit/src/App.jsx
+++ Bookit/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 /* PAGES */
 
@@ -24,6 +24,10 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route element={<AppLayout />}>
+						<Route
+							index
+							element={<Navigate replace to="dashboard" />}
+						/>
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
 						<Route path="cabins" element={<Cabins />} />
