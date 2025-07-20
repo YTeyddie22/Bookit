e82f8b0f35537cf9d4ec49af1a11f2e0ea892c33
YTeyddie22 <teyddie@gmail.com>
2025-07-20 10:43:00 +0300

    feat: implement the routing system

--- Bookit/src/App.jsx
+++ Bookit/src/App.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* PAGES */
+
+import AppLayout from "./pages/AppLayout";
+import GlobalStyles from "./styles/GlobalStyles";
+import Accounts from "./pages/Accounts";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabin";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-    console.log("Hello World");
-    return <p>Hello World</p>;
+	return (
+		<>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="users" element={<Users />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="account" element={<Accounts />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Route>
+
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
