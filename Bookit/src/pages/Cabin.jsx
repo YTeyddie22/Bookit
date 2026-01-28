219a3bbfe998226ad4e5ffcb65b18917298d82d9
YTeyddie22 <teyddie@gmail.com>
2026-01-28 13:11:07 +0300

    (feat): implement cabin

--- Bookit/src/pages/Cabin.jsx
+++ Bookit/src/pages/Cabin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
 import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
@@ -8,10 +9,15 @@ function Cabins() {
 		getCabins().then((data) => console.log(data));
 	}, []);
 	return (
-		<Row type="horizontal">
-			<Heading as="h1">All cabins</Heading>
-			<p>TEST</p>
-		</Row>
+		<>
+			<Row type="horizontal">
+				<Heading as="h1">All cabins</Heading>
+				<p>Filter / Sort</p>
+			</Row>
+			<Row>
+				<CabinTable />
+			</Row>
+		</>
 	);
 }
 
