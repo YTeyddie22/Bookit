d3ad211fd92077b4afbe19d45a54878deda80eca
YTeyddie22 <teyddie@gmail.com>
2026-01-30 13:37:09 +0300

    (feat): implement toasts for alerts

--- Bookit/src/App.jsx
+++ Bookit/src/App.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 /* PAGES */
-
+import { Toaster } from "react-hot-toast";
 import GlobalStyles from "./styles/GlobalStyles";
 import Accounts from "./pages/Accounts";
 import Dashboard from "./pages/Dashboard";
@@ -46,6 +46,24 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
+			<Toaster
+				position="top-center"
+				gutter={12}
+				containerStyle={{ margin: "8px" }}
+				toastOptions={{
+					success: {
+						duration: 3000,
+					},
+					error: 5000,
+					style: {
+						fontSize: "16px",
+						maxWidth: "500px",
+						padding: "16px 24px",
+						backgroundColor: "var(--color-grey-0)",
+						color: "var(--color-grey-700)",
+					},
+				}}
+			/>
 		</QueryClientProvider>
 	);
 }
