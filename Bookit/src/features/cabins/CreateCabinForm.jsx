4f153194bb3af8929507ed06f9284b8dc939edbc
YTeyddie22 <teyddie@gmail.com>
2026-02-09 12:08:19 +0300

    feact(cabin): implement error handlers and on success handlers

--- Bookit/src/features/cabins/CreateCabinForm.jsx
+++ Bookit/src/features/cabins/CreateCabinForm.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
+import { Textarea } from "../../ui/TextArea";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
+
+import { createCabin } from "../../services/apiCabins";
 
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
@@ -8,38 +13,105 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 
-import { Textarea } from "../../ui/TextArea";
-
 // We use react-hook-form to make working with complex and REAL-WORLD forms a lot easier. It handles stuff like user validation and errors. manages the form state for us, etc
 // Validating the user’s data passed through the form is a crucial responsibility for a developer.
 // React Hook Form takes a slightly different approach than other form libraries in the React ecosystem by adopting the use of uncontrolled inputs using ref instead of depending on the state to control the inputs. This approach makes the forms more performant and reduces the number of re-renders.
 
 // Receives closeModal directly from Modal
 function CreateCabinForm({ cabinToEdit, closeModal }) {
+  const queryClient = useQueryClient();
+  const { register, handleSubmit, reset, formState, getValues } = useForm();
+
+  const { errors } = formState;
+  console.log(errors);
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+
+      reset();
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  function submit(data) {
+    mutate(data);
+  }
   return (
-    <Form type="modal">
-      <FormRow label="Cabin name">
+    <Form type="modal" onSubmit={handleSubmit(submit)}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
         {/* register your input into the hook by invoking the "register" function */}
         {/* why the ...? Because this will return an object { onChange, onBlur, customer, ref }, and by spreading we then add all these to the element [show dev tools] */}
         {/* include validation with required or other standard HTML validation rules: required, min, max, minLength, maxLength, pattern, validate */}
         {/* errors will return when field validation fails  */}
-        <Input type="text" id="name" />
+        <Input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Maximum capacity">
-        <Input type="number" id="maxCapacity" />
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register("maxCapacity", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Regular price">
-        <Input type="number" id="regularPrice" />
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Price should be at least 1",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Discount">
-        <Input type="number" id="discount" defaultValue={0} />
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register("discount", {
+            required: "Cannot be empty, at least 0",
+            validate: (value) =>
+              getValues().regularPrice >= value ||
+              "Discount should be less than the regular price",
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Description for website">
-        <Textarea type="number" id="description" defaultValue="" />
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
+        <Textarea
+          type="number"
+          id="description"
+          defaultValue=""
+          {...register("description", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow label="Cabin photo">
@@ -55,7 +127,7 @@ function CreateCabinForm({ cabinToEdit, closeModal }) {
         >
           Cancel
         </Button>
-        <Button>Create new Cabin</Button>
+        <Button disabled={isCreating}>Create new Cabin</Button>
       </FormRow>
     </Form>
   );
